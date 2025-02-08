b33a260e06b5213bd1e65d11b4146b3a8fe8632a
wpiformat <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-08 19:39:22 +0000

    Implemented field cal tool v1.

--- Tools/FieldCal/FieldCal.py
+++ Tools/FieldCal/FieldCal.py
@@ -1,7 +1,13 @@
 import argparse
+import json
+from quaternian_lib import Quaternion, get_quaternion_angle_difference
+from dataclasses import dataclass
 
-import numpy as np
-
+@dataclass
+class Translation:
+    x: float
+    y: float
+    z: float
 
 def proc_args():
     parser = argparse.ArgumentParser(
@@ -10,37 +16,37 @@ def proc_args():
     parser.add_argument("ideal_map")
     parser.add_argument("cal_map")
     args = parser.parse_args()
-    print(args.ideal_map)
-    print(args.cal_map)
 
+    with open(args.ideal_map, 'r') as f:
+        ideal_map_json = json.load(f)
 
-def quaternion_rotation_matrix(Q):
-    # Extract the values from Q
-    q0 = Q[0]
-    q1 = Q[1]
-    q2 = Q[2]
-    q3 = Q[3]
+    with open(args.cal_map, 'r') as f:
+        measured_map_json = json.load(f)
 
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
+    def load_measurements(cal_map):
+        return  [(
+            tag['ID'],
+            Translation(
+                tag['pose']['translation']['x'],
+                tag['pose']['translation']['y'],
+                tag['pose']['translation']['z']),
+            Quaternion(
+                tag['pose']['rotation']['quaternion']['W'],
+                tag['pose']['rotation']['quaternion']['X'],
+                tag['pose']['rotation']['quaternion']['Y'],
+                tag['pose']['rotation']['quaternion']['Z']))
+                for tag in cal_map['tags']]
+    ideal_map_measurements = sorted(load_measurements(ideal_map_json), key=lambda x: x[0])
+    calibrated_map_measurements = sorted(load_measurements(measured_map_json), key=lambda x: x[0])
 
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
+    map_pairs = zip(ideal_map_measurements, calibrated_map_measurements)
 
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02], [r10, r11, r12], [r20, r21, r22]])
-
-    return rot_matrix
-
+    for ideal, cal in map_pairs:
+        print(f"ID: {ideal[0]}")
+        print(f"X: {ideal[1].x - cal[1].x}")
+        print(f"Y: {ideal[1].y - cal[1].y}")
+        print(f"Z: {ideal[1].z - cal[1].z}")
+        print(f"Angle: {get_quaternion_angle_difference(ideal[2], cal[2])}\n")
 
 if __name__ == "__main__":
     proc_args()
