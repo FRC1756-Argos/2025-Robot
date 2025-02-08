f54ae6de8dec5fe37c2a181bfef188a6f4ebe89d
Eternal2008 <Eternal2008@users.noreply.github.com>
2025-02-08 11:51:32 -0600

    initial framework for field calibration

---
+++ Tools/FieldCal/FieldCal.py
@@ -0,0 +1,45 @@
+import argparse
+import json
+import numpy as np
+
+
+def proc_args():
+    parser = argparse.ArgumentParser(prog="quaternion \"stuff\"",
+                                     description='ideal to calibrated field')
+    parser.add_argument("ideal_map")
+    parser.add_argument("cal_map")
+    args = parser.parse_args()
+    print(args.ideal_map)
+    print(args.cal_map)
+
+
+def quaternion_rotation_matrix(Q):
+    # Extract the values from Q
+    q0 = Q[0]
+    q1 = Q[1]
+    q2 = Q[2]
+    q3 = Q[3]
+
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+
+    # 3x3 rotation matrix
+    rot_matrix = np.array([[r00, r01, r02], [r10, r11, r12], [r20, r21, r22]])
+
+    return rot_matrix
+
+
+if __name__ == "__main__":
+    proc_args()
---
+++ Tools/FieldCal/setup.bat
@@ -0,0 +1,3 @@
+pip install uv
+uv venv
+uv pip install -r Requirements.txt
