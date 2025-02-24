cb3de12a8e79db1af0ccec7a311f55158239fa81
David K Turner <dkt01@users.noreply.github.com>
2025-02-24 16:42:48 +0000

    Move implementation for SetTrajectoryDisplay to cpp file Clear trajectory before adding new trajectory Replace deprecated pigeon GetRate() API

--- src/main/include/subsystems/swerve_drive_subsystem.h
+++ src/main/include/subsystems/swerve_drive_subsystem.h
@@ -265,12 +265,7 @@ class SwerveDriveSubsystem : public frc2::SubsystemBase {
 
   void SimDrive();
 
-  void SetTrajectoryDisplay(const std::vector<frc::Pose2d>& trajectory) {
-    for (size_t i = 0; i < trajectory.size(); i++) {
-      std::string name = "Waypoint " + std::to_string(i);
-      m_field.GetObject(name)->SetPose(trajectory[i]);
-    }
-  }
+  void SetTrajectoryDisplay(const std::vector<frc::Pose2d>& trajectory);
 
  private:
   argos_lib::RobotInstance m_instance;
