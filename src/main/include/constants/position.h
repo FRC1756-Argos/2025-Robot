1cc40173854a9a99d45de8986de60b96d2165b2e
GHatten <97795242+GHatten@users.noreply.github.com>
2025-02-26 06:22:26 +0000

    Apply automatic formatting

--- src/main/include/constants/position.h
+++ src/main/include/constants/position.h
@@ -36,8 +36,7 @@ namespace setpoints {
   constexpr Position levelTwoRight{.elevator_height = 21_in, .arm_angle = 134_deg, .wrist_angle = 0_deg};
   constexpr Position levelTwoLeft{.elevator_height = 21_in, .arm_angle = 49_deg, .wrist_angle = 0_deg};
   constexpr Position levelThreeRight{.elevator_height = 38_in, .arm_angle = 134_deg, .wrist_angle = 0_deg};
-  constexpr Position levelThreeLeft{.elevator_height = 38_in, .arm_angle = 49_deg
-  , .wrist_angle = 0_deg};
+  constexpr Position levelThreeLeft{.elevator_height = 38_in, .arm_angle = 49_deg, .wrist_angle = 0_deg};
   constexpr Position levelFourRight{.elevator_height = 61.25_in, .arm_angle = 130_deg, .wrist_angle = 0_deg};
   constexpr Position levelFourLeft{.elevator_height = 61.25_in, .arm_angle = 58_deg, .wrist_angle = 0_deg};
   constexpr Position levelFourCenter{.elevator_height = 61.25_in, .arm_angle = 90_deg, .wrist_angle = 0_deg};
