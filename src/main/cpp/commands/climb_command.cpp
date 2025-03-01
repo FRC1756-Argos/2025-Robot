e430b0e8cb51d54e7b4f2ae4b204af9502ab26c1
Logan Anderson <logrand9239@gmail.com>
2025-02-28 18:20:06 -0600

    added break overlap with winch command

--- src/main/cpp/commands/climb_command.cpp
+++ src/main/cpp/commands/climb_command.cpp
@@ -10,8 +10,8 @@ ClimbCommand::ClimbCommand(ClimberSubsystem* climberSubsystem) : m_pClimberSubsy
 
 // Called when the command is initially scheduled.
 void ClimbCommand::Initialize() {
-  if (m_pClimberSubsystem->ClimberGetAngle() < 30_deg) {
-    m_pClimberSubsystem->ClimberMoveToAngle(30_deg);
+  if (m_pClimberSubsystem->ClimberGetAngle() < 25_deg) {
+    m_pClimberSubsystem->ClimberMoveToAngle(25_deg);
   }
 }
 
@@ -20,8 +20,11 @@ void ClimbCommand::Execute() {
   if (m_pClimberSubsystem->GetClimberManualOverride()) {
     Cancel();
   }
-  if (m_pClimberSubsystem->ClimberGetAngle() >= 29_deg) {
+  if (m_pClimberSubsystem->ClimberGetAngle() >= 25_deg) {
     m_pClimberSubsystem->WinchIn();
+  }
+  if (m_pClimberSubsystem->ClimberGetAngle() >= 30_deg) {
+    m_pClimberSubsystem->SetPositionMotorBreakModeToBreak(false);
   }
 }
 
