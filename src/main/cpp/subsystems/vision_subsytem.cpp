a2987e61ca99c4250510d046fd052734d68b24c5
dkt01 <dkt01@users.noreply.github.com>
2025-03-04 07:53:28 -0600

    Still log megatag2 odoemetry for telemetry

--- src/main/cpp/subsystems/vision_subsytem.cpp
+++ src/main/cpp/subsystems/vision_subsytem.cpp
@@ -42,6 +42,23 @@ VisionSubsystem::VisionSubsystem(const argos_lib::RobotInstance instance, Swerve
       "hb",
       [this](double) {
         LimelightHelpers::PoseEstimate mt2 = LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2(leftCameraTableName);
+        if (mt2.tagCount > 0 &&
+            units::math::abs(m_pDriveSubsystem->GetIMUYawRate()) < units::degrees_per_second_t{360}) {
+          /// @todo Get good odometry from vision
+          // units::meter_t avgDist{mt2.avgTagDist};
+          // const auto time =
+          //     units::second_t{ctre::phoenix6::utils::GetCurrentTimeSeconds()} - units::millisecond_t{mt2.latency};
+          // if (mt2.tagCount > 2 && avgDist < 15_ft) {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {.2, .2, 9999999.0});
+          // } else if (mt2.tagCount > 2 && avgDist < 25_ft) {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {.6, .6, 9999999.0});
+          // } else if (mt2.tagCount > 2 || avgDist < 15_ft) {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {.9, .9, 9999999.0});
+          // } else {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {10.0, 10.0, 9999999.0});
+          // }
+          m_rightCameraMegaTag2PoseLogger.Append(mt2.pose, units::microsecond_t{mt2.timestampSeconds}.to<int64_t>());
+        }
       },
       -1);
   m_rightCameraFrameUpdateSubscriber.AddMonitor(
@@ -49,6 +66,23 @@ VisionSubsystem::VisionSubsystem(const argos_lib::RobotInstance instance, Swerve
       [this](double) {
         LimelightHelpers::PoseEstimate mt2 =
             LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2(rightCameraTableName);
+        if (mt2.tagCount > 0 &&
+            units::math::abs(m_pDriveSubsystem->GetIMUYawRate()) < units::degrees_per_second_t{360}) {
+          /// @todo Get good odometry from vision
+          // units::meter_t avgDist{mt2.avgTagDist};
+          // const auto time =
+          //     units::second_t{ctre::phoenix6::utils::GetCurrentTimeSeconds()} - units::millisecond_t{mt2.latency};
+          // if (mt2.tagCount > 2 && avgDist < 15_ft) {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {.2, .2, 9999999.0});
+          // } else if (mt2.tagCount > 2 && avgDist < 25_ft) {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {.6, .6, 9999999.0});
+          // } else if (mt2.tagCount > 2 || avgDist < 15_ft) {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {.9, .9, 9999999.0});
+          // } else {
+          //   m_pDriveSubsystem->UpdateVisionMeasurement(mt2.pose, time, {10.0, 10.0, 9999999.0});
+          // }
+          m_leftCameraMegaTag2PoseLogger.Append(mt2.pose, units::microsecond_t{mt2.timestampSeconds}.to<int64_t>());
+        }
       },
       -1);
   m_yawUpdateThread = std::jthread(std::bind_front(&VisionSubsystem::UpdateYaw, this));
