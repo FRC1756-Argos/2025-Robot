63116292236ae562251e619c8efc63ffc31fd6f1
Eternal2008 <122846197+Eternal2008@users.noreply.github.com>
2025-03-05 10:01:08 -0600

    144 make l1 fe place auto (#166)
    * Created Choreo path
    
    * Made path shorter and reduced turning
    
    * Created L1 FE Auto command and added to selector
    
    * Apply automatic formatting
    
    * Assign number 04 for L1 FE
    
    * Apply automatic formatting
    
    * Fix copyright and add event to move arm
    
    ---------
    
    Co-authored-by: Eternal2008 <Eternal2008@users.noreply.github.com>
    Co-authored-by: David K Turner <dkt01@users.noreply.github.com>
    Co-authored-by: dkt01 <6155678+dkt01@users.noreply.github.com>

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -58,12 +58,19 @@ RobotContainer::RobotContainer()
     , m_climberSubSystem(m_instance)
     , m_intakeSubSystem(m_instance)
     , m_autoNothing{m_swerveDrive}
+    , m_autoL1FE{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoChoreoTest{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoForward{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL1GH{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL1IJ{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL4G{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
-    , m_autoSelector{{&m_autoNothing, &m_autoForward, &m_autoL1GH, &m_autoL4G, &m_autoL1IJ, &m_autoChoreoTest},
+    , m_autoSelector{{&m_autoNothing,
+                      &m_autoForward,
+                      &m_autoL1GH,
+                      &m_autoL4G,
+                      &m_autoL1FE,
+                      &m_autoL1IJ,
+                      &m_autoChoreoTest},
                      &m_autoNothing}
     , m_transitionedFromAuto{false} {
   // Initialize all of your commands and subsystems here
---
+++ src/main/cpp/commands/autonomous/autonomous_L1_FE.cpp
@@ -0,0 +1,60 @@
+/// @copyright Copyright (c) Argos FRC Team 1756.
+///            Open Source Software; you can modify and/or share it under the terms of
+///            the license file in the root directory of this project.
+
+#include "commands/autonomous/autonomous_L1_FE.h"
+
+#include <frc2/command/InstantCommand.h>
+#include <frc2/command/ParallelCommandGroup.h>
+#include <frc2/command/SequentialCommandGroup.h>
+#include <frc2/command/WaitCommand.h>
+#include <units/length.h>
+
+#include "commands/autonomous/auto_utils.h"
+#include "commands/drive_choreo.h"
+#include "commands/go_to_position_command.h"
+
+AutonomousL1FE::AutonomousL1FE(ElevatorSubsystem& elevator,
+                               IntakeSubsystem& intake,
+                               SwerveDriveSubsystem& swerve,
+                               VisionSubsystem& vision)
+    : m_Elevator{elevator}
+    , m_Intake{intake}
+    , m_Swerve{swerve}
+    , m_Vision{vision}
+    , m_armPositionEventCallback{[this](ArmPosition position) {
+      auto_utils::SetAutoArmPosition(position, &m_Elevator, &m_Intake);
+    }}
+    , m_allCommands{
+          frc2::SequentialCommandGroup{DriveChoreo{m_Swerve, "L1_FE", true, m_armPositionEventCallback},
+                                       frc2::InstantCommand([this]() { m_Intake.Outtake(0.15); }, {&m_Intake}),
+                                       frc2::WaitCommand(300_ms),
+                                       GoToPositionCommand(&m_Elevator, setpoints::stow)}} {}
+
+// Called when the command is initially scheduled.
+void AutonomousL1FE::Initialize() {
+  m_allCommands.Initialize();
+}
+
+// Called repeatedly when this Command is scheduled to run
+void AutonomousL1FE::Execute() {
+  m_allCommands.Execute();
+}
+
+// Called once the command ends or is interrupted.
+void AutonomousL1FE::End(bool interrupted) {
+  m_allCommands.End(interrupted);
+}
+
+// Returns true when the command should end.
+bool AutonomousL1FE::IsFinished() {
+  return m_allCommands.IsFinished();
+}
+
+std::string AutonomousL1FE::GetName() const {
+  return "04. L1 FE";
+}
+
+frc2::Command* AutonomousL1FE::GetCommand() {
+  return dynamic_cast<frc2::Command*>(this);
+}
