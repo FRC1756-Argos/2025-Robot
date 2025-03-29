1786d35224b6a290f15b7dd88fe5af670854917c
Logan Anderson <logrand9239@gmail.com>
2025-03-29 09:31:43 -0500

    Added intake map

--- src/main/include/constants/interpolation_maps.h
+++ src/main/include/constants/interpolation_maps.h
@@ -19,6 +19,8 @@ namespace controllerMap {
       InterpMapPoint{-1.0, -0.7}, InterpMapPoint{-0.15, 0.0}, InterpMapPoint{0.15, 0.0}, InterpMapPoint{1.0, 0.7}};
   [[maybe_unused]] constexpr std::array driveSpeed_placing{
       InterpMapPoint{-1.0, -0.3}, InterpMapPoint{-0.15, 0.0}, InterpMapPoint{0.15, 0.0}, InterpMapPoint{1.0, 0.3}};
+  [[maybe_unused]] constexpr std::array driveSpeed_intake{
+      InterpMapPoint{-1.0, -0.3}, InterpMapPoint{-0.15, 0.0}, InterpMapPoint{0.15, 0.0}, InterpMapPoint{1.0, 0.3}};
   [[maybe_unused]] constexpr std::array driveRotSpeed{
       InterpMapPoint{-1.0, -1.0}, InterpMapPoint{-0.15, 0.0}, InterpMapPoint{0.15, 0.0}, InterpMapPoint{1.0, 1.0}};
   [[maybe_unused]] constexpr std::array driveRotSpeed_placing{
