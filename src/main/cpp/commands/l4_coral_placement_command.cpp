189e3a49fae4f9268df7d5cdbe6210aa7a40b361
piscijac <piscijac@gmail.com>
2025-04-04 19:15:49 -0500

    add elevator move down in L4 place command

--- src/main/cpp/commands/l4_coral_placement_command.cpp
+++ src/main/cpp/commands/l4_coral_placement_command.cpp
@@ -5,12 +5,13 @@
 #include "commands/l4_coral_placement_command.h"
 
 L4CoralPlacementCommand::L4CoralPlacementCommand(ElevatorSubsystem* elevatorSubsystem, IntakeSubsystem* intakeSubsystem)
-    : m_pElevatorSubsystem{elevatorSubsystem}, m_pIntakeSubsystem{intakeSubsystem} {
+    : m_pElevatorSubsystem{elevatorSubsystem}, m_pIntakeSubsystem{intakeSubsystem}, m_placed{false} {
   AddRequirements({m_pElevatorSubsystem, m_pIntakeSubsystem});
 }
 
 // Called when the command is initially scheduled.
 void L4CoralPlacementCommand::Initialize() {
+  m_placed = false;
   auto currentArmPosition = m_pElevatorSubsystem->GetArmAngle();
   if (currentArmPosition < 90_deg) {
     m_pElevatorSubsystem->ArmMoveToAngle(currentArmPosition - 47_deg);
@@ -27,13 +28,13 @@ void L4CoralPlacementCommand::Execute() {
     Cancel();
   }
 
-  if (m_pElevatorSubsystem->IsArmAtSetPoint() &&
+  if(m_placed && m_pElevatorSubsystem->IsAtSetPoint() ) {
+    m_pElevatorSubsystem->ElevatorMoveToHeight(m_pElevatorSubsystem->GetElevatorHeight() - 4_in);
+  } else if (!m_placed && m_pElevatorSubsystem->IsArmAtSetPoint() &&
       (std::chrono::steady_clock::now() - m_startTime) >= std::chrono::milliseconds(100)) {
     m_pElevatorSubsystem->ElevatorMoveToHeight(m_pElevatorSubsystem->GetElevatorHeight() + 2.5_in);
   }
-  if ((std::chrono::steady_clock::now() - m_startTime) >= std::chrono::milliseconds(500)) {
-    m_pIntakeSubsystem->Outtake(0.5);
-  }
+
   m_pIntakeSubsystem->Outtake(0.02);
 }
 
@@ -42,11 +43,21 @@ void L4CoralPlacementCommand::End(bool interrupted) {
   if (interrupted) {
     m_pElevatorSubsystem->GoToPosition(m_pElevatorSubsystem->GetPosition());
   }
+  else {
+    m_pElevatorSubsystem->ElevatorMoveToHeight(m_pElevatorSubsystem->GetElevatorHeight() - 4_in);
+  }
   m_pIntakeSubsystem->Stop();
 }
 
 // Returns true when the command should end.
 bool L4CoralPlacementCommand::IsFinished() {
-  return m_pElevatorSubsystem->IsAtSetPoint() &&
+  if (!m_placed) {
+    m_placed = m_pElevatorSubsystem->IsAtSetPoint() &&
          (std::chrono::steady_clock::now() - m_startTime) >= std::chrono::milliseconds(500);
+    if(m_placed) {
+      m_startTime = std::chrono::steady_clock::now();
+    }
+  }
+  return m_placed && m_pElevatorSubsystem->IsAtSetPoint() &&
+         (std::chrono::steady_clock::now() - m_startTime) >= std::chrono::milliseconds(200);
 }
