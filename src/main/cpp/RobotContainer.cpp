9b7cae1e75413887e3413349cbc2088ede221df5
dkt01 <dkt01@users.noreply.github.com>
2025-04-17 09:12:51 -0500

    Add (untested) L4G Algae auto routine using vision

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -67,6 +67,7 @@ RobotContainer::RobotContainer()
     , m_autoL1GH{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL1IJ{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL4GAlgae{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
+    , m_autoL4GAlgaeVision{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL4G{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL4EDC{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
     , m_autoL4JKL{m_elevatorSubSystem, m_intakeSubSystem, m_swerveDrive, m_visionSubSystem}
@@ -85,6 +86,7 @@ RobotContainer::RobotContainer()
                       &m_autoL1L4EDC,
                       &m_autoL1L4JKL,
                       &m_autoL4GAlgae,
+                      &m_autoL4GAlgaeVision,
                       &m_autoChoreoTest},
                      &m_autoNothing}
     , m_transitionedFromAuto{false} {
